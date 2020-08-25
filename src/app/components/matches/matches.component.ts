2000d4fc569e03bd38d81d0b8428f77df49a416a
Rami <rami.sassi.pro@gmail.com>
2020-08-25 23:51:07 +0100

    winner matches + front design

--- src/app/components/matches/matches.component.ts
+++ src/app/components/matches/matches.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./matches.component.css']
 })
 export class MatchesComponent implements OnInit {
+  // number of matches
+  numberOfMatches = 0;
+
+  // the minimum and maximum amount of matches that to generate
+  minMatches = 10;
+  maxMatches = 30;
+
+  // state of the game
+  winner = false;
+
+  // current player
+  currentPlayer = 1;
 
   constructor() { }
 
   ngOnInit() {
+    // random generation of the number of matches
+    // by default Math.random() return value between 0 and 1, for that we multiply ==> Math.random() * (this.maxMatches - this.minMatches) + this.minMatches
+    this.numberOfMatches = Math.floor(Math.random() * (this.maxMatches - this.minMatches) + this.minMatches);
+  }
+
+  launchGame(numberOfMatches) {
+    // removal of matches (decrement)
+    this.numberOfMatches -= numberOfMatches;
+
+    // check the end condition
+    if (this.numberOfMatches <= 0) {
+      this.numberOfMatches = 0;
+      this.winner = true;
+    }
+
+    if (this.winner === false) {
+      // end of the round
+      if (this.currentPlayer === 2) {
+        this.currentPlayer = 1;
+      } else {
+        this.currentPlayer = 2;
+      }
+    }
   }
 
 }
